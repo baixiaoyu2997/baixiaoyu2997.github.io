0c1819a815120d1037d97de2f04b088df461d6a9
L.Rain <bxy2997@sina.com>
2019-03-13 19:02:55 +0800

    修改配置文件

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,19 @@
 cp -rf "E:/Nutstore/private/博客/." "src/_posts/"
 echo "拷贝博客到此项目"
 set -e
-yarn build &
-git clone git@github.com:baixiaoyu2997/baixiaoyu2997.github.io.git &
-wait
-echo "生成静态页面,拉取gh-pages项目"
-cp -rf "./dist/." "./baixiaoyu2997.github.io"
-cd ./baixiaoyu2997.github.io
+yarn build
+echo "生成静态页面"
+git stash push -u -m 储藏
+git checkout -q master
+git stash pop
+cd dist
+mv * ../
+rm dist
 git add -A
 git commit -m upload
 git push origin master
-cd ..
 echo "上传博客"
-rm -rf baixiaoyu2997.github.io
+git checkout -q dev
 git reset --hard
 git clean -df
 echo "重置主仓库"
