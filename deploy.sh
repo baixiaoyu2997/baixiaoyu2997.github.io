ea5070c2373fb2150338d9421cd1bfe8a439c0d5
L.Rain <bxy2997@sina.com>
2019-03-13 19:12:06 +0800

    修改配置文件

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
 
 cp -rf "E:/Nutstore/private/博客/." "src/_posts/"
+read -p "推送成功，按回车键退出" var
 echo "拷贝博客到此项目"
 set -e
 yarn build
+rm -rf "src/_posts/"
 echo "生成静态页面"
+read -p "推送成功，按回车键退出" var
 git stash push -u -m 储藏
 git checkout -q master
 git stash pop
 cd dist
 mv * ../
-rm dist
+rm -rf dist
 git add -A
 git commit -m upload
 git push origin master
